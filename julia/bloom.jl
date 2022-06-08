0fbc7806503609b0c4fc0236a6620b4de5b5f803
Alessandrostr95 <alessandrostr95@gmail.com>
2022-06-08 15:50:18 +0200

    started julia version of tracer.py

---
+++ julia/bloom.jl
@@ -0,0 +1,54 @@
+using SpecialFunctions
+
+# "airy disk" function (actually approximate, and a rescaling, but it's ok)
+# see https://en.wikipedia.org/wiki/Bessel_function#Bessel_functions_of_the_first_kind:_J%CE%B1
+airy_disk(x::AbstractArray) = (2 * besselj1.(x) ./ x).^2
+
+# generate a (2*size+1,2*size+1) convolution kernel with "radii" scale
+# where the function above is assumed to have "radius" one
+# scale is a 3-vector for RGB
+function generate_kernel(scale::AbstractArray, s::Int64)
+    x = -s:s
+    y = -s:s
+
+    xs, ys = meshgrid(x, y)
+
+    kernel = zeros(size(xs)[1], 3, size(xs)[2])
+
+    r = sqrt.(xs.^2 + ys.^2) .+ 0.000001
+    kernel = airy_disk(reshape(r, 21, 1, 21) ./ reshape(scale, 1,3,1))
+
+    #normalization
+    kernel ./= reshape(([kernel[:, i, :] for i = 1:3] .|> sum), 1,3,1)
+
+    return kernel
+end
+
+const SPECTRUM = [1., 0.86, 0.61]
+
+function airy_convolve(array, radius::Float64, kernel_radius=25)
+    kernel = generate_kernel(radius * SPECTRUM , kernel_radius)
+
+    out = zeros(size(array)[1], 3, size(array)[2])
+    for i in range(3)
+        out[:, i, :] = convolve2d(array[:, i, :], kernel[:, i, :], mode='same', boundary='symm')
+    end 
+    return out
+end
+
+
+######
+function meshgrid(x,y)
+    nx = length(x)
+    ny = length(y)
+    X = zeros(ny, nx)
+    Y = zeros(ny, nx)
+
+    for j=1:nx
+        for ix=1:ny
+            X[i, j]= x[j]
+            Y[i, j]= y[i]
+        end
+    end
+    return X, Y
+end
--- tracer.py
+++ tracer.py
@@ -6,6 +6,9 @@ import scipy.ndimage as ndim
 import scipy.misc as spm
 import random,sys,time,os
 import datetime
+import imageio
+
+from skimage.transform import rescale
 
 import multiprocessing as multi
 import ctypes
@@ -313,7 +316,8 @@ def rgbtosrgb(arr):
     arr[mask] **= 1/2.4
     arr[mask] *= 1.055
     arr[mask] -= 0.055
-    arr[-mask] *= 12.92
+    #arr[-mask] *= 12.92
+    arr[~mask] *= 12.92
 
 
 # convert from srgb to linear rgb
@@ -323,12 +327,14 @@ def srgbtorgb(arr):
     arr[mask] += 0.055
     arr[mask] /= 1.055
     arr[mask] **= 2.4
-    arr[-mask] /= 12.92
+    #arr[-mask] /= 12.92
+    arr[~mask] /= 12.92
 
 
 logger.debug("Loading textures...")
 if SKY_TEXTURE == 'texture':
-    texarr_sky = spm.imread('textures/bgedit.jpg')
+    # texarr_sky = spm.imread('textures/bgedit.jpg')
+    texarr_sky = imageio.imread('textures/bgedit.jpg')
     # must convert to float here so we can work in linear colour
     texarr_sky = texarr_sky.astype(float)
     texarr_sky /= 255.0
@@ -338,16 +344,20 @@ if SKY_TEXTURE == 'texture':
     if not LOFI:
         #   maybe doing this manually and then loading is better.
         logger.debug("(zooming sky texture...)")
-        texarr_sky = spm.imresize(texarr_sky,2.0,interp='bicubic')
+        # texarr_sky = spm.imresize(texarr_sky,2.0,interp='bicubic')
+        texarr_sky = rescale(texarr_sky, 2.0)  
+
         # imresize converts back to uint8 for whatever reason
         texarr_sky = texarr_sky.astype(float)
         texarr_sky /= 255.0
 
 texarr_disk = None
 if DISK_TEXTURE == 'texture':
-    texarr_disk = spm.imread('textures/adisk.jpg')
+    # texarr_disk = spm.imread('textures/adisk.jpg')
+    texarr_disk = imageio.imread('textures/adisk.jpg')
 if DISK_TEXTURE == 'test':
-    texarr_disk = spm.imread('textures/adisktest.jpg')
+    # texarr_disk = spm.imread('textures/adisktest.jpg')
+    texarr_disk = imageio.imread('textures/adisktest.jpg')
 if texarr_disk is not None:
     # must convert to float here so we can work in linear colour
     texarr_disk = texarr_disk.astype(float)
